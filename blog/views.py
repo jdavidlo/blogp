faa76607710828a3abf75aaa44106114239282ed
jdavidlo <jdibicho@gmail.com>
2020-04-27 23:04:06 -0500

    dinamico

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import DatosB
 
 # Create your views here.
 def datos_list(request):
-    return render(request, 'blog/datos_list.html', {})
+    posts = DatosB.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/datos_list.html', {'posts': posts})
